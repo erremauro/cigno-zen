58119039c1a806cf550c0fb54b5366dfe83600b3
Roberto Mauro <roberto.mauro@icloud.com>
2025-08-06 09:43:12 +0200

    Add a welcome message to page-home.php

--- pages/page-home.php
+++ pages/page-home.php
@@ -1,4 +1,25 @@
+<section class="home-section" id="welcome">
+<div class="post-content">
+<h2 class="wp-heading">Benvenuti</h2>
+
+<p>Questo sito ospita un'estesa collezione di traduzioni in italiano della tradizione buddhista zen e non solo.</p>
+</div>
+</section>
+
 <?php get_template_part('patterns/template-home-articles'); ?>
 <?php get_template_part('patterns/template-home-authors'); ?>
 <?php get_template_part('patterns/template-home-volumes'); ?>
 <?php get_template_part('patterns/template-home-categories'); ?>
+
+<section class="home-section" id="gift-of-dhamma">
+	<div style="text-align: center; margin: 4em 0 2em 0">
+		<?php get_template_part('parts/separator'); ?>
+	</div>
+	<div class="post-content">
+		<h2 class="wp-heading has-text-align-center">Il Dono del Dhamma</h2>
+
+		<p>Tutto il Dhamma qui esposto è offerto gratuitamente, senza pubblicità o donazioni suggerite. Questa è la diffusione del Dhamma secondo il Dhamma, in linea con quanto il Buddha insegnò riguardo a come i suoi insegnamenti dovessero essere offerti.</p>
+
+		<p>Per coloro che vogliono esprimere gratitudine per la generosità dei maestri, il modo migliore è mettere in pratica gli insegnamenti, con impegno sincero — per vedere con i propri occhi, per il proprio beneficio e per il beneficio di tutti gli esseri.</p>
+	</div>
+</section>
