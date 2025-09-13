7f62feaa59325a71721f2dbd00b9993d980e1125
Roberto Mauro <roberto.mauro@icloud.com>
2025-09-13 22:52:16 +0200

    Custom message created for Masters not found in "404.php"

--- 404.php
+++ 404.php
@@ -1,14 +1,24 @@
 <?php get_template_part('parts/header'); ?>
 
+
 <div id="primary" class="content-area">
-	<main id="main" class="site-main">
-		<section class="error-404 not-found">
-			<h1 class="ku">空</h1>
-			<p><em>La forma è vuoto, il vuoto è forma</em></p>
-			<p class="info">La pagina che stavi cercando non esiste.</p>
-			<p><a href="/">Ritorna alla homepage</a></p>
-		</section>
-	</main>
+    <main id="main" class="site-main">
+        <section class="error-404 not-found">
+            <?php if (strpos($_SERVER['REQUEST_URI'], '/maestro') === 0): ?>
+                <h1 class="ku">無</h1>
+                <p><em>Maestro non trovato</em></p>
+                <p class="info">La biografia del Maestro che stavi cercando non esiste ancora.</p>
+                <p>
+                    <a href="javascript:history.back()" class="btn-back">← Torna indietro</a>
+                </p>
+            <?php else: ?>
+                <h1 class="ku">空</h1>
+                <p><em>La forma è vuoto, il vuoto è forma</em></p>
+                <p class="info">La pagina che stavi cercando non esiste.</p>
+                <p><a href="/">Ritorna alla homepage</a></p>
+            <?php endif ?>
+        </section>
+    </main>
 </div>
 
 <?php get_template_part('parts/footer'); ?>
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.6.2] 2025-09-13
+### Changed
+- Custom message created for Masters not found in `404.php`
+
 ## [1.6.1] 2025-09-13
 ### Fixed
 - Fix tag-cloud pill box-shadow for light theme
@@ -227,7 +231,8 @@ the slug's name
 - Initial `index.php`, `style.css`, and `functions.php`
 - This Changelog
 
-[Unreleased]: https://github.com/erremauro/cigno-zen/compare/v1.6.1...HEAD
+[Unreleased]: https://github.com/erremauro/cigno-zen/compare/v1.6.2...HEAD
+[1.6.2]: https://github.com/erremauro/cigno-zen/releases/tag/v1.6.2
 [1.6.1]: https://github.com/erremauro/cigno-zen/releases/tag/v1.6.1
 [1.6.0]: https://github.com/erremauro/cigno-zen/releases/tag/v1.6.0
 [1.5.0]: https://github.com/erremauro/cigno-zen/releases/tag/v1.5.0
