b2c2dcb2fe2bf7555c22addcbb2a822760150aeb
Roberto Mauro <erremauro@icloud.com>
2026-07-20 08:55:54 +0200

    Hides dictionary's term source information.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 ### Changed
-- Suble changes has been applied to the featured articles in the homepage
+- Hides dictionary's term source information
+- Subtle changes has been applied to the featured articles in the homepage
 
 ## [2.7.5] 2026-06-17
 ### Changed
--- parts/tags-header.php
+++ parts/tags-header.php
@@ -49,11 +49,13 @@ $panel_id = 'tag-full-description-' . $term->term_id;
 				</div>
 			<?php endif; ?>
 
-			<?php if ( $definition_source ) : ?>
+			<!-- Nasconde la fonte della definizione
+			<?php /* if ( $definition_source ) : */ ?>
 				<div class="definition-source">
-					<p>"<?php echo esc_html( $definition_source ); ?>"</p>
+					<p>"<?php /* echo esc_html( $definition_source ); */ ?>"</p>
 				</div>
-			<?php endif; ?>
+			<?php /* endif; */ ?>
+			-->
 		</div>
 	<?php endif; ?>
 </header>
